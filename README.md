da1408f338c31812f7e21402affc6656f4d92d70
Kam <kameronkales@gmail.com>
2019-05-29 12:32:20 -0400

    updated to remove hardcoded env variables

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-So, I don't endorse scraping LinkedIn but wrote this based on what someone in the group asked for.
+## Summary
 
-Use at your own peril
+I wrote this to automate exporting your connections on LinkedIn. I was asked to write this from a member of my previous startups facebook group, Recruiting Leaders.
+
+I learned how to bypass getting detected while using Selenium and how to select elements whose classes are dynamically generated.
+
+This code uses:
+
+- Python 2.7
+- Selenium
+- LinkedIn
+- Environment Variables:
+  - LINKEDIN_LOGIN_EMAIL - please export before running code
+  - LINKEDIN PASSWORD - please export before running code.
+
+---
--- export.py
+++ export.py
@@ -2,34 +2,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time as t
+import os
 from selenium.webdriver.common.action_chains import ActionChains
 
-## Go to this website (https://chromedriver.storage.googleapis.com/index.html?path=2.33/)
-## and download the correct chromedriver
-## for your machine. After downloading, put the driver in this repo.
+# Go to this website (https://chromedriver.storage.googleapis.com/index.html?path=2.33/)
+# and download the correct chromedriver
+# for your machine. After downloading, put the driver in this repo.
 
 browser = webdriver.Chrome('./chromedriver')
 link = 'https://www.linkedin.com'
 browser.get(link)
 t.sleep(5)
 login_bar = browser.find_element_by_id('login-email')
-## login_bar.send_keys('INSERT YOUR LOG IN EMAIL BELOW INSIDE THESE QUOTES')
-login_bar.send_keys('')
+login_bar.send_keys(os.environ.get("LINKEDIN_LOGIN_EMAIL", None))
 t.sleep(5)
 password_bar = browser.find_element_by_id('login-password')
-## password_bar.send_keys('INSERT YOUR PASSWORD BELOW INSIDE THESE QUOTES')
-password_bar.send_keys('')
+password_bar.send_keys(os.environ.get("LINKEDIN_PASSWORD", None))
 t.sleep(5)
 login_bar.submit()
 browser.get("https://www.linkedin.com/psettings/member-data")
 t.sleep(5)
-export_data = browser.find_element_by_class_name("data-export-option-label").click()
+export_data = browser.find_element_by_class_name(
+    "data-export-option-label").click()
 t.sleep(5)
 export_button = browser.find_element_by_id("download-button").click()
 t.sleep(5)
 verify_password = browser.find_element_by_id("verify-password")
-## enter_password = verify_password.send_keys('INSERT YOUR PASSWORD HERE LIKE YOU DID ABOVE')
-enter_password = verify_password.send_keys('')
+enter_password = verify_password.send_keys(
+    os.environ.get("LINKEDIN_PASSWORD", None))
 t.sleep(5)
 enter_button = browser.find_element_by_class_name("submit")
 enter_button.submit()
